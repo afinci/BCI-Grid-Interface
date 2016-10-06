bfa1f4f61b73f694fa12ad4592ecabc8edb069ee
Ahmet Fatih Inci <ahmetfatihinci@hotmail.com>
2016-10-06 16:06:22 +0300

    2D Grid Interface for ErrP Detection

---
+++ grid2way/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace grid2way
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(944, 541);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(288, 162);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(1100, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 76);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TRIAL: ";
+            this.label1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(558, 142);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Location = new System.Drawing.Point(705, 41);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox5.Location = new System.Drawing.Point(437, 89);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(70, 70);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(944, 540);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Interface ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ grid2way/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace grid2way
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        private extern static IntPtr LoadLibrary(String DllName);
+
+        [DllImport("kernel32.dll")]
+        private extern static IntPtr GetProcAddress(IntPtr hModule, String ProcName);
+
+        [DllImport("kernel32.dll")]
+        private extern static bool FreeLibrary(IntPtr hModule);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate bool InitializeWinIoType();
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate void SetPortValType(UInt16 PortAddr, UInt32 PortVal, UInt16 Size);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate bool ShutdownWinIoType();
+
+        IntPtr hMod;
+
+        UInt16 address = 16376; //Port address of Lenovo T530 for PCI Parallel Port, check EEG Based BCI Experiments Manual for further info
+
+        int trial = 100;             // total number of trials
+        int keyLimit = 2;       // 2 sec wait after press
+        int keyCounter = 0; 
+        int showLimit = 2;      // 2 sec show selected response  - timer2
+        int showCounter = 0;
+        int keyEvent = 0;       // which direction
+        int position = 0;       // 4 veya -4 se 0 la
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown); 
+
+            var height = Screen.PrimaryScreen.Bounds.Height;
+            var width = Screen.PrimaryScreen.Bounds.Width;
+            
+            pictureBox1.Height = height;
+            pictureBox3.Height = height / 10;
+            pictureBox2.Height = height / 10;
+            pictureBox4.Height = height / 10;
+
+            pictureBox5.Height = height / 10;
+            pictureBox5.Width = width / 10;
+
+            pictureBox1.Width = width;
+            pictureBox3.Width = width / 10;
+            pictureBox2.Width = width / 10;
+            pictureBox4.Width = width / 10;
+
+            pictureBox3.Location = new Point((width / 2)-80, (height / 2) - 50);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Check pointer size to determine dll type
+            if (IntPtr.Size == 4)
+            {
+                hMod = LoadLibrary("WinIo32.dll");
+            }
+            else if (IntPtr.Size == 8)
+            {
+                hMod = LoadLibrary("WinIo64.dll");
+            }
+            //Error Message
+            if (hMod == IntPtr.Zero)
+            {
+                MessageBox.Show("Can't find WinIo dll");
+                this.Close();
+            }
+            IntPtr pFunc = GetProcAddress(hMod, "InitializeWinIo");
+            if (pFunc != IntPtr.Zero)
+            {
+                InitializeWinIoType InitializeWinIo = (InitializeWinIoType)Marshal.GetDelegateForFunctionPointer(pFunc, typeof(InitializeWinIoType));
+                bool Result = InitializeWinIo();
+                if (!Result)
+                {
+                    MessageBox.Show("Error returned from InitializeWinIo.\nMake sure you are running with administrative privileges and that the WinIo library files are located in the same directory as your executable file.");
+                    FreeLibrary(hMod);
+                    this.Close();
+                }
+            }
+
+    //        label1.Text = "TRIAL: " + trial;
+
+            IntPtr send = GetProcAddress(hMod, "SetPortVal");
+            SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+            SetPortVal(address, 50, 1);
+        }
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            var height = Screen.PrimaryScreen.Bounds.Height;
+            var width = Screen.PrimaryScreen.Bounds.Width;
+            
+            var x = (width / 2) - 80;
+            var y = (height / 2) - 50;
+        
+            if (e.KeyCode == Keys.Left)
+            {
+                IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                SetPortVal(address, 100, 1);
+
+                keyEvent = 0; // left
+                timer1.Start();
+
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                SetPortVal(address, 150, 1);
+
+                keyEvent = 1; // right 
+                timer1.Start();
+            }
+  
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            keyCounter = keyCounter + 1;
+
+            if (keyCounter >= keyLimit)
+            {
+                timer1.Stop();
+                keyCounter = 0;
+
+                Random rnd = new Random();
+                int result = rnd.Next(10);
+                trial--;
+
+                if (keyEvent == 0) // left 
+                {
+                    if (result >= 2) // CORRECT DIRECTION 
+                    {
+                        position--;
+                        pictureBox3.Left -= 180;
+                  //      pictureBox4.Visible = true;
+                  //      pictureBox4.Location = pictureBox3.Location;
+
+                              pictureBox5.Visible = true;
+                             pictureBox5.Location = pictureBox3.Location;
+
+                             IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                             SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                             SetPortVal(address, 120, 1);
+                    }
+
+                    else // WRONG DIRECTION 
+                    {
+                        position++;
+                        pictureBox3.Left += 180;
+                      //  pictureBox2.Visible = true;
+                    //    pictureBox2.Location = pictureBox3.Location;
+
+                        pictureBox5.Visible = true;
+                        pictureBox5.Location = pictureBox3.Location;
+                        IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                        SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                        SetPortVal(address, 180, 1);
+                    }
+                }
+
+                else if (keyEvent == 1) //right
+                {
+                    if (result >= 2) // CORRECT DIRECTION 
+                    {
+                        pictureBox3.Left += 180;
+                        position++;
+                   //     pictureBox4.Visible = true;
+                   //     pictureBox4.Location = pictureBox3.Location;
+
+                        pictureBox5.Visible = true;
+                        pictureBox5.Location = pictureBox3.Location;
+
+                        IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                        SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                        SetPortVal(address, 120, 1);
+                      
+                    }
+                    else // WRONG DIRECTION 
+                    {
+                        pictureBox3.Left -= 180;
+                        position--;
+                    //    pictureBox2.Visible = true;
+                   //     pictureBox2.Location = pictureBox3.Location;
+
+                        pictureBox5.Visible = true;
+                        pictureBox5.Location = pictureBox3.Location;
+
+                        IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                        SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                        SetPortVal(address, 180, 1);
+                    }
+                }
+
+                timer2.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            showCounter = showCounter + 1;
+            var height = Screen.PrimaryScreen.Bounds.Height;
+            var width = Screen.PrimaryScreen.Bounds.Width;
+
+            if (showCounter >= showLimit)
+            {
+                timer2.Stop();
+            //    pictureBox2.Visible = false;
+             //   pictureBox4.Visible = false;
+                pictureBox5.Visible = false;
+       
+                showCounter = 0;
+
+                if (position <= -4 || position >= 4)
+                {
+                    pictureBox3.Location = new Point((width / 2) - 80, (height / 2) - 50);
+                    position = 0;
+                }
+
+            //    label1.Text = "TRIAL: " + trial;
+
+                IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                SetPortVal(address, 50, 1);
+
+                if (trial <= 0)
+                {
+                    SetPortVal(address, 250, 1);
+                    Application.Exit();
+                }
+            }
+        }    
+    }
+}
